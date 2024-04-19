c7e3111ac47c765fd07474bc32b63d8a466139f1
hvtraanf <vhoangtran99@gmail.com>
2024-04-19 10:34:12 +0700

    reformat

--- models/ToysModel.js
+++ models/ToysModel.js
@@ -1,23 +1,23 @@
 var mongoose = require('mongoose');
 var ToysSchema = mongoose.Schema(
-   {
-      name: {
-        type : String,
-        required: true
-      },
-      price: {
-        type: Number,
-        required: true
-      },
-      quantity:{
-        type: Number,
-        required: true
-      },
-      imageUrl: {
-        type: String,
-        required: true
-      }
-   }
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true
+    },
+    imageUrl: {
+      type: String,
+      required: true
+    }
+  }
 );
 
 const ToysModel = mongoose.model("toys", ToysSchema);
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,17 +1,17 @@
 var mongoose = require('mongoose');
 
 var UserSchema = mongoose.Schema(
-   {
-      username: {
-        type : String,
-        required: true
-      },
-      password: {
-        type: String,
-        required: true,
-        minLength: 8
-      }
-   }
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8
+    }
+  }
 );
 
 var UserSchema = mongoose.model('users', UserSchema);
